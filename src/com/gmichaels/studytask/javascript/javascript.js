22e4920dd32ef810a3e5814dd2b855f733f32917
G.Michael.S <gmsgithub@gmail.com>
2016-02-13 22:00:56 +0300

    Update Project. JavaScript

---
+++ src/com/gmichaels/studytask/javascript/javascript.js
@@ -0,0 +1,7 @@
+function change_color() {
+
+    var x = document.getElementById("test");
+    x.style.fontSize = "25px";
+    x.style.color = "green";
+
+}
--- src/com/gmichaels/studytask/php/index.php
+++ src/com/gmichaels/studytask/php/index.php
@@ -1,5 +1,15 @@
-<?php
-
-    echo 'StudyTask!';
-
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Пример веб-страницы</title>
+        <link rel="stylesheet" href="../css/style.css">
+        <script src="../javascript/javascript.js"></script>
+    </head>
+    <body>
+        <h1 id="test">Заголовок</h1>
+        <input type="button" onclick="change_color(this.form)" value="Change Color!"/>
+        <p class="test">Первый абзац.</p>
+        <p class="test">Второй абзац.</p>
+    </body>
+</html>
