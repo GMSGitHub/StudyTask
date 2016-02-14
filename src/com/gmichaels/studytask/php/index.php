ad843c03c08fce0e2f36cf03ddb2079b4f0066f5
G.Michael.S <gmsgithub@gmail.com>
2016-02-14 20:51:49 +0300

    Update Project. HTML

--- src/com/gmichaels/studytask/php/index.php
+++ src/com/gmichaels/studytask/php/index.php
@@ -6,26 +6,47 @@
         <link rel="stylesheet" type="text/css" href="../css/style.css"/>
     </head>
     <body>
-        <form class="main">
-            <button class="buttons">
-                <a href="insert.php">INSERT</a>
-            </button>
-            <hr>
-            <button class="buttons">
-                <a href="select.php">SELECT</a>
-            </button>
-            <hr>
-            <button class="buttons">
-                <a href="update.php">UPDATE</a>
-            </button>
-            <hr>
-            <button class="buttons">
-                <a href="delete.php">DELETE</a>
-            </button>
-            <hr>
-            <button class="buttons">
-                <a href="../html/lab1.html">Task Lab #1</a>
-            </button>
-        </form>
+        <div class="main">
+            <form class="main">
+                <hr>
+                <h2 align="center">Tasks List</h2>
+                <hr>
+                <button class="buttons">
+                    <a href="../html/lab1.html">Task Lab #1</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="../html/lab2.html">Task Lab #2</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="../html/lab3.html">Task Lab #3</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="../html/lab4.html">Task Lab #4</a>
+                </button>
+            </form>
+            <form class="main">
+                <hr>
+                <h2 align="center">CRUD</h2>
+                <hr>
+                <button class="buttons">
+                    <a href="insert.php">INSERT</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="select.php">SELECT</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="update.php">UPDATE</a>
+                </button>
+                <hr>
+                <button class="buttons">
+                    <a href="delete.php">DELETE</a>
+                </button>
+            </form>
+        </div>
     </body>
 </html>
